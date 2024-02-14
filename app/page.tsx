ba2d2c86c3f7f948372e3193b6a4cfa5160f9bd7
BrunoUA96 <ostapklysa@gmail.com>
2024-02-14 16:16:47 +0000

    install axios

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,16 @@
+const API_TOKEN = process.env.API_TOKEN;
+
 export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Hello World</h1>
+      <ul>
+        {/* {paintings.map((painting) => (
+          <div key={painting.id}>
+            <h1>{painting.title}</h1>
+          </div>
+        ))} */}
+      </ul>
     </main>
   );
 }
