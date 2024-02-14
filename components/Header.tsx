7bd01e84230ca28ce5fcc0fa5f4f61e16edb0bd5
BrunoUA96 <ostapklysa@gmail.com>
2024-02-14 18:08:26 +0000

    create base header

---
+++ components/Header.tsx
@@ -0,0 +1,108 @@
+"use client";
+
+import Link from "next/link";
+import { Menu, Transition } from "@headlessui/react";
+import { Fragment, useState } from "react";
+
+export const Header = () => {
+  const [logged, setLogged] = useState(false);
+
+  return (
+    <header className="bg-[#FFB63B] flex justify-between items-center py-4 px-9">
+      <Link href="/">Logo</Link>
+      <nav className="flex items-center gap-9">
+        <Link href="/gallery">Gallery</Link>
+        <Link href="/about">About</Link>
+        <Menu as="div" className="relative inline-block text-left">
+          <div>
+            <Menu.Button className="inline-flex w-full justify-center rounded-md bg-black/20 px-4 py-2 text-sm font-medium text-white hover:bg-black/30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75">
+              {logged ? "Profile" : "Sign/Login"}
+            </Menu.Button>
+          </div>
+          <Transition
+            as={Fragment}
+            enter="transition ease-out duration-100"
+            enterFrom="transform opacity-0 scale-95"
+            enterTo="transform opacity-100 scale-100"
+            leave="transition ease-in duration-75"
+            leaveFrom="transform opacity-100 scale-100"
+            leaveTo="transform opacity-0 scale-95">
+            {logged ? (
+              <Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none">
+                <div className="px-1 py-1 ">
+                  <Menu.Item>
+                    {({ active }) => (
+                      <Link
+                        href="/profile"
+                        className={`${
+                          active ? "bg-violet-500 text-white" : "text-gray-900"
+                        } group flex w-full items-center rounded-md px-2 py-2 text-sm`}>
+                        {active ? "" : ""}
+                        Profile
+                      </Link>
+                    )}
+                  </Menu.Item>
+                  <Menu.Item>
+                    {({ active }) => (
+                      <Link
+                        href="/favorites"
+                        className={`${
+                          active ? "bg-violet-500 text-white" : "text-gray-900"
+                        } group flex w-full items-center rounded-md px-2 py-2 text-sm`}>
+                        {active ? "" : ""}
+                        Favorites
+                      </Link>
+                    )}
+                  </Menu.Item>
+                </div>
+
+                <div className="px-1 py-1">
+                  <Menu.Item>
+                    {({ active }) => (
+                      <button
+                        className={`${
+                          active ? "bg-violet-500 text-white" : "text-gray-900"
+                        } group flex w-full items-center rounded-md px-2 py-2 text-sm`}>
+                        {active ? "" : ""}
+                        Logout
+                      </button>
+                    )}
+                  </Menu.Item>
+                </div>
+              </Menu.Items>
+            ) : (
+              <Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none">
+                <div className="px-1 py-1 ">
+                  <Menu.Item>
+                    {({ active }) => (
+                      <Link
+                        href="/sign-in"
+                        className={`${
+                          active ? "bg-violet-500 text-white" : "text-gray-900"
+                        } group flex w-full items-center rounded-md px-2 py-2 text-sm`}>
+                        {active ? "" : ""}
+                        Sign in
+                      </Link>
+                    )}
+                  </Menu.Item>
+                  <Menu.Item>
+                    {({ active }) => (
+                      <Link
+                        href="/sign-up"
+                        className={`${
+                          active ? "bg-violet-500 text-white" : "text-gray-900"
+                        } group flex w-full items-center rounded-md px-2 py-2 text-sm`}>
+                        {active ? "" : ""}
+                        Sign up
+                      </Link>
+                    )}
+                  </Menu.Item>
+                </div>
+              </Menu.Items>
+            )}
+          </Transition>
+        </Menu>
+      </nav>
+    </header>
+  );
+};
