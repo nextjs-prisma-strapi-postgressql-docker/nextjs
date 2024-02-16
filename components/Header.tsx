b8badecece841f4044c2e26a4747d2d9d207c754
BrunoUA96 <ostapklysa@gmail.com>
2024-02-16 16:54:16 +0000

    check session

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,9 +3,11 @@
 import Link from "next/link";
 import { Menu, Transition } from "@headlessui/react";
 import { Fragment, useState } from "react";
+import { useSession } from "next-auth/react";
 
 export const Header = () => {
-  const [logged, setLogged] = useState(false);
+  // Get user session
+  const { data: session } = useSession();
 
   return (
     <header className="bg-[#FFB63B] flex justify-between items-center py-4 px-9">
@@ -16,7 +18,7 @@ export const Header = () => {
         <Menu as="div" className="relative inline-block text-left">
           <div>
             <Menu.Button className="inline-flex w-full justify-center rounded-md bg-black/20 px-4 py-2 text-sm font-medium text-white hover:bg-black/30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75">
-              {logged ? "Profile" : "Sign/Login"}
+              {session ? "Profile" : "Sign/Login"}
             </Menu.Button>
           </div>
           <Transition
@@ -27,7 +29,7 @@ export const Header = () => {
             leave="transition ease-in duration-75"
             leaveFrom="transform opacity-100 scale-100"
             leaveTo="transform opacity-0 scale-95">
-            {logged ? (
+            {session ? (
               <Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none">
                 <div className="px-1 py-1 ">
                   <Menu.Item>
@@ -76,7 +78,7 @@ export const Header = () => {
                   <Menu.Item>
                     {({ active }) => (
                       <Link
-                        href="/sign-in"
+                        href="/auth/sign-in"
                         className={`${
                           active ? "bg-violet-500 text-white" : "text-gray-900"
                         } group flex w-full items-center rounded-md px-2 py-2 text-sm`}>
